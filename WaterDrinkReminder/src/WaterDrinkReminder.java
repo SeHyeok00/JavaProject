6558ab8d5c243d52f66f873554db1ec59e241901
SonSeHyeok <sonsh7041@naver.com>
2025-05-18 02:58:14 +0900

    기본 메뉴판

---
+++ WaterDrinkReminder/src/WaterDrinkReminder.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WaterDrinkReminder {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        for (;;) {
+            printMainMenu();
+            String input = sc.nextLine();
+        }
+    }
+
+    static void printMainMenu() {
+        System.out.println("물 마시기 프로그램!!💦");
+        System.out.println("1. 프로그램 시작");
+        System.out.println("2. 기록 보기");
+        System.out.println("3. 설정");
+        System.out.print("선택: ");
+    }
+}
